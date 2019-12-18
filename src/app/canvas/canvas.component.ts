0348985afd469d4d1667c56aec3a6f25354c4230
Aled Wassell <aledwassell@Aleds-MacBook-Pro.local>
2019-12-18 07:25:37 +0000

    remove console logs

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -37,16 +37,11 @@ export class CanvasComponent implements OnInit {
   }
 
   private createCanvas() {
-    console.log(this.canvas.nativeElement.offsetWidth);
-    console.log(this.canvas.nativeElement.offsetHeight);
     this.p5 = new P5(this.sketch, this.canvas.nativeElement);
   }
 
   private sketch(p: any) {
     p.setup = () => {
-      console.log(p.windowWidth);
-      console.log(p.windowHeight);
-      console.log(p);
       p.createCanvas(p.windowWidth, p.windowHeight);
       p.background(255);
     };
@@ -57,7 +52,6 @@ export class CanvasComponent implements OnInit {
 
   private drawFunction = () => {
     this.p5.mouseDragged = () => {
-      console.log('mousedragged');
       this.p5.fill(COLOR);
       this.p5.noStroke();
       this.p5.ellipse(this.p5.mouseX, this.p5.mouseY, 10, 10);
